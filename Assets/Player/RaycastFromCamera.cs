0796afa42876dba7dbad53fe19a48ac6c9da70f3
Kaleb Rice <pdxrice04@gmail.com>
2021-12-29 01:35:54 -0800

    Added Destructible Objects
    Pogging

---
+++ Assets/Player/RaycastFromCamera.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastFromCamera : MonoBehaviour
+{
+    [SerializeField] private Camera playerCamera;
+
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, 300.0f);
+            Transform objectHit = hit.collider.transform;
+            if (objectHit != null)
+            {
+                Debug.Log(objectHit.name);
+                if (objectHit.tag == "Joint")
+                {
+                    Destroy(objectHit.gameObject);
+                }
+            }
+            
+            Debug.Log("Ray has been cast");
+        }
+    }
+}
