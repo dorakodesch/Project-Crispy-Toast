d071cbc8ed21cca8c71ddf3bcce00778ae76f260
Jesse Mitchell-Reiss <j.mitchell-reiss@outlook.com>
2022-01-05 10:55:16 -0800

    Fix Input System & Update to Input System Package

---
+++ Assets/Player/toolFunctions.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toolFunctions : MonoBehaviour
+{
+    // Create variables for object in hand
+    public enum inHand { hammer, grapple };
+    public inHand current;
+    public int objIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = inHand.hammer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        changeInHand();
+    }
+
+    // Change object function
+    public void changeInHand()
+    {
+
+    }
+}
