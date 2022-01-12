2c787fea07ea19fc796f0bd428ee3568c98ad584
dorian <dkodesch8292@student.pps.net>
2022-01-12 11:43:18 -0800

    basic robot navigation

---
+++ Assets/Robot/RobotNavigation.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class RobotNavigation : MonoBehaviour
+{
+	[SerializeField]
+	Transform player;
+
+	NavMeshAgent agent;
+
+	private void Start()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	private void Update()
+	{
+		agent.SetDestination(player.position + Vector3.right);
+	}
+}
