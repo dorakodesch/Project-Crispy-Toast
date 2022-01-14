da0cd92658799ad48d2d42cd6b6db778792df5b6
Jesse Mitchell-Reiss <j.mitchell-reiss@outlook.com>
2022-01-14 13:03:40 -0800

    destructible objects integrated

---
+++ Assets/Destructable Objects/DestructableChunk.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructableChunk : MonoBehaviour
+{
+    [SerializeField] private GameObject[] importantJoints;
+
+    private bool jointsGone = true;
+    private Rigidbody chunkRB;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        jointsGone = true;
+
+        for (int ii = 0; ii < importantJoints.Length; ii++)
+        {
+            if (importantJoints[ii] != null)
+            {
+                jointsGone = false;
+            }
+        }
+        if (jointsGone)
+        {
+            gameObject.transform.parent = null;
+            chunkRB = gameObject.AddComponent<Rigidbody>();
+        }
+    }
+}
