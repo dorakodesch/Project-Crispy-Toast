da2d2468fc8fa490b2716d6a92b2baa10447e41c
Kaleb Rice <pdxrice04@gmail.com>
2022-01-15 22:40:42 -0800

    Began adding particles to joints

---
+++ Assets/joint.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class joint : MonoBehaviour
+{
+    private void OnDestroy()
+    {
+        //Trigger particle system
+    }
+}
