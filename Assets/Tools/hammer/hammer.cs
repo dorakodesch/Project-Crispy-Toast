6e9f605eabe05db575389911248ab1ea653e271f
Kaleb Rice <pdxrice04@gmail.com>
2022-01-26 14:09:30 -0800

    Random changes GitKraken is holding me hostage over
    Send help

--- Assets/Tools/hammer/hammer.cs
+++ Assets/Tools/hammer/hammer.cs
@@ -15,13 +15,11 @@ public class hammer : MonoBehaviour
     // Destroy objects from hammer hit with fire
     public void Fire(Ray forward, Vector3 position, Quaternion rotation)
     {
-        Debug.Log("hit from hammer");
         RaycastHit hit;
         Physics.Raycast(forward, out hit, 1000.0f);
         Transform objectHit = hit.collider.transform;
         if (objectHit != null)
         {
-            Debug.Log(objectHit.name);
             if (objectHit.tag == "Joint")
             {
                 Destroy(objectHit.gameObject);
