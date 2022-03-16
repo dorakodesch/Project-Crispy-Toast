55b2caa477110c2e57ba0b1b8f45ed7ec078d18a
dorian <dkodesch8292@student.pps.net>
2022-03-16 12:09:23 -0700

    target setup

---
+++ Assets/Robot/RobotDestination.cs
@@ -0,0 +1,8 @@
+// A* robot target (easier to set its position than have the robot move to a set position)
+using UnityEngine;
+
+public class RobotDestination : MonoBehaviour
+{
+	[SerializeField]
+	Transform player;
+}
