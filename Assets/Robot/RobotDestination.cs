adefbe36fcdb90a76fe0336850ae56bcc3d4778a
dorian <dkodesch8292@student.pps.net>
2022-03-28 14:03:29 -0700

    obstacle and breakable object checks

--- Assets/Robot/RobotDestination.cs
+++ Assets/Robot/RobotDestination.cs
@@ -6,8 +6,45 @@ public class RobotDestination : MonoBehaviour
 	[SerializeField]
 	Transform player;
 
+	// int in case we need >2 states later
+	int state;
+
+	DestructableChunk[] chunks;
+
+	private void Start()
+	{
+		// following player at the beginning
+		state = 0;
+	}
+
 	private void Update()
 	{
-		transform.position = player.position;
+		UpdateState();
+
+		// following player
+		if (state == 0)
+		{
+			transform.position = player.position;
+		}
+		// following chunk
+		else
+		{
+
+		}
+	}
+
+	private void UpdateState()
+	{
+		state = 0;
+		chunks = FindObjectsOfType<DestructableChunk>();
+
+		foreach (DestructableChunk i in chunks)
+		{
+			if (i.jointsGone)
+			{
+				state = 1;
+				break;
+			}
+		}
 	}
 }
